5fe4ca3d99a28f489807941e1cba9cf7065a352a
diegofleitas <diego@design-it.de>
2019-11-29 09:40:48 -0300

    revoved evilcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeroesComponent } from './components/heroes/heroes.component';
-import { EvilComponentComponent } from './components/evil-component/evil-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
-    EvilComponentComponent
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
