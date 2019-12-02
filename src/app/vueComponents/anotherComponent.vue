c0a444d21e0028adc87c80444fa041f8c96167f2
alejox21 <alejox21.12@gmail.com>
2019-12-02 16:01:21 -0300

    vueComponents folder and files

---
+++ src/app/vueComponents/anotherComponent.vue
@@ -0,0 +1,45 @@
+<template>
+    <div style="border: 3px dashed green; margin-top:1em;">
+        <h3>This is "anotherComponent" (vueComponent's child)!</h3>
+            <transition-group name="bounce" tag="ul">
+                <li v-for="item in messages" v-bind:key="item">
+                    {{item}}
+                </li>
+            </transition-group>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            messages: [],
+        },
+        data(){
+            return{
+                messegesarr: [],
+            }
+        },
+        created() {
+            console.log(this.messages)
+        },
+    }
+</script>
+<style>
+.bounce-enter-active {
+  animation: bounce-in .5s;
+}
+.bounce-leave-active {
+  animation: bounce-in .5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(2.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
---
+++ src/app/vueComponents/vueComponent.vue
@@ -0,0 +1,43 @@
+
+<template>
+  <div style="border: 3px dashed green; padding: 5px; text-align:center">
+    <h3>I'm a demo Vue tag</h3>
+      {{'this is a message: '+msg}}
+    <br />
+
+    <input style="font-size:1.2em;" type="text" class="form-control" id="alt-image" v-model="text">
+    <button style="font-size:1.2em; background:green; color:white;" @click="sendMessage" class="btn btn-info">Send message to son</button>
+    
+    <another-component :messages="messages" :key="messages.length"></another-component>
+
+  </div>
+</template>
+
+<script>
+import anotherComponent from "./anotherComponent.vue";
+
+export default {
+  props: [
+    'msg'
+  ],
+  components: {
+    anotherComponent
+  },
+  created() {
+    // alert('Hello from the Vue side!');
+  },
+  data() {
+    return {
+      text:'',
+      parentmessage: '',
+      messages: []
+    }
+  },
+  methods: {
+    sendMessage() {
+        this.messages.push(this.text);
+        this.text = '';
+    }
+  }
+};
+</script>
