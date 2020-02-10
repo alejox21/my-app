c2a0074e711acb720f279aec6c7909e078c07825
alejox21 <alejox21.12@gmail.com>
2020-02-10 12:14:08 -0300

    vuex implemented and working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './components/heroes/heroes.component';
-import { TasksComponent } from './components/tasks/tasks.component'
+import { TasksComponent } from './components/tasks/tasks.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
